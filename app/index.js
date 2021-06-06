5ab8531a9fc8ce423528034c2e586e7400f173c2
andydevs <akanshul97@gmail.com>
2021-06-05 20:46:54 -0400

    Set up canvas

--- app/index.js
+++ app/index.js
@@ -7,4 +7,12 @@
 import './style/main.scss'
 
 // Bundle script
-console.log('yoyoyoyoyo')
+let canvas = document.querySelector('#mycanvas')
+let ctx = canvas.getContext('2d')
+
+// M'draw some stuff
+ctx.clearRect(0, 0, canvas.width, canvas.height)
+ctx.beginPath()
+ctx.moveTo(canvas.width/2, 3*canvas.height/4)
+ctx.lineTo(canvas.width/2 - 100, 3*canvas.height/4 - 100)
+ctx.stroke()
